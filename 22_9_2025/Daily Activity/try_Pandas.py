0e512c780c8dd0b45d72a2a4ea86b0b90f280ff9
Soham7021 <nimbalkarss123@gmail.com>
2025-09-22 15:45:40 +0530

    commit

--- 22_9_2025/Daily Activity/try_Pandas.py
+++ 22_9_2025/Daily Activity/try_Pandas.py
@@ -1,12 +1,34 @@
 import pandas as pd
-
+import numpy as np
 data = {
-    "name":["Soham","Sahil","Rahul"],
-    "age":[22,19,37],
-    "course":["AIML","MBBS","Law"],
-    "skills":["AI","ML","Python"],
-    "marks":[50,70,45]
+    "Name": ["Rahul", "Priya", "Arjun", "Neha", "Vikram"],
+    "Age": [21, 22, 20, 23, 21],
+    "Course": ["AI", "ML", "Data Science", "AI", "ML"],
+    "Marks": [85, 90, 78, 88, 95]
 }
 
 df = pd.DataFrame(data)
+print(df)
+print(" ")
+#selecting data
+
+print(df["Name"])
+print(" ")
+print(df[["Name", "Marks"]]) # Multiple COlumns
+print(" ")
+print(df.iloc[0]) #first row
+print(" ")
+print(df.loc[2,"Marks"]) #value at row 2 column Marks
+
+print(" ")
+#Filer data
+high_scorers = df[df["Marks"] > 80]
+print(high_scorers)
+
+#Where clause
+
+df["Result"] = np.where(df["Marks"] >= 80, "Pass","Fail")
+
+df.loc[df["Name"]=="Neha", "Marks"] = 92
+
 print(df)
