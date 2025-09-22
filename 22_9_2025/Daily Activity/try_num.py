4171560ee64938d42e26280fd3447aac7bec396a
Soham7021 <nimbalkarss123@gmail.com>
2025-09-22 14:36:53 +0530

    numpy file added

---
+++ 22_9_2025/Daily Activity/try_num.py
@@ -0,0 +1,12 @@
+import numpy as np
+n1 = np.array([1, 2, 3])
+n2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+print(n1)
+print(n2)
+print(n1.max())
+print(n1.min())
+print(n1.mean())
+print(n1[:2])
+print(n2[::-1])
+print(n1.sum(n1))
