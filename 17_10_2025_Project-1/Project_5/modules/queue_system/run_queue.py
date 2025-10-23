1392550d3006b6540361ece4210f275d67998cac
Soham7021 <nimbalkarss123@gmail.com>
2025-10-23 10:28:41 +0530

    Stop tracking .env inside 17_10_2025_Project-1

--- 17_10_2025_Project-1/Project_5/modules/queue_system/run_queue.py
+++
@@ -1,7 +0,0 @@
-# modules/queue_module/run_queue_demo.py
-from producer import push_updates
-from consumer import process_updates
-
-if __name__ == "__main__":
-    push_updates()
-    process_updates()
